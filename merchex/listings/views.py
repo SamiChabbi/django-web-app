9674eb68e3f7874ee980270288e46a88e580ef6a
Sami Chabbi <sami.chabbi.42@gmail.com>
2024-05-06 17:27:28 +0200

    first commit

---
+++ merchex/listings/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse  
+
+
+def hello(request):
+    return HttpResponse("<h1>Hello world</h1>")
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
